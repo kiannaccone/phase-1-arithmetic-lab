c04728210cde17be9b36a040a58deeb47b6b2d33
Katherine Iannaccone <katherineiannaccone@yahoo.com>
2021-09-22 10:57:36 -0600

    commit tree b20b3d8b117ee0a88c974580c7bbd59de2090cc1

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2; 
 
-// Write your code here
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 32
+let num4 = 8
+const mod = num3 / num4;
+
+const max = Math.max(1, 2, 3, 4, 5, 20);
